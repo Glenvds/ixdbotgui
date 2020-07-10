aafec3becc063a3b84a141493223598e1f8ed867
Ruben Van Impe <rubenvi673@gmail.com>
2020-07-10 23:00:03 +0200

    First draft

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject, BehaviorSubject, interval } from "rxjs";
+import { Observable, Subject, BehaviorSubject, interval, of } from "rxjs";
 import { IMusicData, IBotState } from "../models/queue-info";
 
 @Injectable({
@@ -15,7 +15,7 @@ export class ApiService {
     constructor(
         private readonly httpClient: HttpClient
     ) {
-        interval(3000).subscribe(async () => {
+        interval(6000).subscribe(async () => {
 
             const info = await this.getInfo$().toPromise();
             if (!this.currentState && !info) return;
@@ -27,7 +27,15 @@ export class ApiService {
     }
 
     getInfo$(): Observable<IBotState> {
-        return this.httpClient.get<IBotState>("http://localhost:8080/");
+        const mock : IBotState = {timestamp: new Date(), musicData:[
+            {title: 'Kendrick Lamar - Swimming Pools (Drank)', url:'www.whatever.com', requester:{displayAvatarURL:'https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png', username: 'Koby'}},
+            {title: 'Pharoahe Monch - Simon Says [High Quality]', url:'www.whatever', requester:{displayAvatarURL:'https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png', username: 'Smolders'}},
+            {title: 'ScHoolboy Q - Floating ft. 21 Savage', url:'www.whatever', requester:{displayAvatarURL:'https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png', username: 'Gay Jerre'}},
+            {title: 'NCT 127 엔시티 127 Simon Says MV', url:'www.whatever', requester:{displayAvatarURL:'https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png', username: 'Karla'}},
+        ] }       
+        //return this.httpClient.get<IBotState>("http://192.168.0.100:8080/");
+
+        return of(mock)
     }
 
     listenToInfoUpdate(): Observable<IBotState> {
